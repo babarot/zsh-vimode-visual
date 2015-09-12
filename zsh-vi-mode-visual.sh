b130eeb693256a3f6976cb836d008631c19fb4a2
b4b4r07 <b4b4r07@gmail.com>
2015-09-12 23:40:55 +0900

    Add License

--- zsh-vi-mode-visual.sh
+++ zsh-vi-mode-visual.sh
@@ -1,5 +1,23 @@
 #!/bin/zsh -f
 
+#===============================================================================
+# LICENCE: GNU GPL version 3
+#
+# zle_vi_visual.zsh is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This project is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#===============================================================================
+
 get-x-clipboard() {
     if which pbpaste >/dev/null 2>&1; then
         alias clippaste='pbpaste'
